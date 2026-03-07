b5bdad5e4cb13d9bc465caa81520bee5a70b1938
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-07 15:13:24 +0100

    Update routes.js

--- utils/routes.js
+++ utils/routes.js
@@ -1,15 +1,7 @@
 // utils/routes.js
+
 export const appHashLink = (path) => {
   if (!path) return "#/";
   if (!path.startsWith("/")) path = "/" + path;
   return `/#${path}`;
 };
-
-// components/HashLink.js
-import React from "react";
-
-export default function HashLink({ to, ...props }) {
-  if (!to) to = "/";
-  const hashTo = to.startsWith("/#") ? to : `/#${to}`;
-  return <a href={hashTo} {...props} />;
-}
