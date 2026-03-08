9e1c96c3887fe260bd0b36f7976132d975896933
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-08 18:00:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,18 +8,19 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then((reg) => console.log("Service worker registered", reg))
-      .catch((err) => console.log("Service worker failed", err));
-  });
-}
+<React.StrictMode>
+<AuthProvider>
+<App />
+</AuthProvider>
+</React.StrictMode>
 );
+
+// Register Service Worker
+if ("serviceWorker" in navigator) {
+window.addEventListener("load", () => {
+navigator.serviceWorker
+.register("/service-worker.js")
+.then((reg) => console.log("Service worker registered", reg))
+.catch((err) => console.log("Service worker failed", err));
+});
+}
