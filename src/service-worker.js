f882e79e4598630dd8aa4dea1fd9f05d62d711c7
Chris <106016563+Xtosea@users.noreply.github.com>
2026-03-08 14:25:54 +0100

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,11 +1,4 @@
-self.addEventListener("install", (event) => {
-  console.log("Service Worker installing.");
-});
+import { precacheAndRoute } from 'workbox-precaching';
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// This line is required by Workbox
+precacheAndRoute(self.__WB_MANIFEST);
