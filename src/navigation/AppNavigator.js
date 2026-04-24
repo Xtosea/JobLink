388c54b1a49fc3184345287d56d95d9f4b846ef1
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-24 15:21:51 +0100

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -10,8 +10,21 @@ export default function AppNavigator() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
+
+      {/* 🆕 Jobs Flow */}
+      <Stack.Screen name="Jobs" component={Jobs} />
+      <Stack.Screen
+        name="JobDetails"
+        component={JobDetails}
+        options={{ title: "Job Details" }}
+      />
+
       <Stack.Screen name="About" component={About} />
-      <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} options={{ title: "Privacy Policy" }} />
+      <Stack.Screen
+        name="PrivacyPolicy"
+        component={PrivacyPolicy}
+        options={{ title: "Privacy Policy" }}
+      />
     </Stack.Navigator>
   );
 }
