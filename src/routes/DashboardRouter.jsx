0b9fc0e3d7aa651884f691c5d3d54183822c344c
Chris <106016563+Xtosea@users.noreply.github.com>
2026-04-25 19:43:28 +0100

    Create routes/DashboardRouter.jsx

---
+++ src/routes/DashboardRouter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import EmployerDashboard from "../pages/EmployerDashboard";
+import ApplicantDashboard from "../pages/ApplicantDashboard";
+import AdminDashboard from "../pages/AdminDashboard";
+
+export function DashboardRouter() {
+  let user = null;
+
+  try {
+    user = JSON.parse(localStorage.getItem("user"));
+  } catch (err) {
+    user = null;
+  }
+
+  if (!user || !user.role) {
+    return <div>Please login</div>;
+  }
+
+  if (user.role === "employer") {
+    return <EmployerDashboard />;
+  }
+
+  if (user.role === "applicant") {
+    return <ApplicantDashboard />;
+  }
+
+  if (user.role === "admin") {
+    return <AdminDashboard />;
+  }
+
+  return <div>Invalid role</div>;
+}
